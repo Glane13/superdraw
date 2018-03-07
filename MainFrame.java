cfdaae42fb3bcc58c7e68064fc32823ae3c2678e
JonasMogilevec <jonas.mogilevec@online.liverpool.ac.uk>
2018-03-07 17:54:22 +0000

    Reverted to the most functional version of the code.

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,8 @@
 package superdraw;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
@@ -10,10 +12,9 @@ public class MainFrame extends JFrame {
     String selection;
     protected JPanel cards = new JPanel(new CardLayout());
     protected CardLayout cl = (CardLayout)(cards.getLayout());
-    private JColorChooser ColourChooser = new JColorChooser();
-    public Color myColour = Color.BLUE;
-    public Color newColor;
-    
+    private Color myColour = Color.BLACK;
+    private Color c = Color.BLUE;
+
 
     public MainFrame () {
         JFrame myFrame = new JFrame("Super Draw Application");
@@ -22,51 +23,15 @@ public class MainFrame extends JFrame {
 
         JPanel myPanel = new JPanel();
 
-        //JButton myButton = new JButton("Choose Colour");
-        //myButton.setPreferredSize(new Dimension(100, 75));
+        JButton colourButton = new JButton("Choose Colour");
+        colourButton.setPreferredSize(new Dimension(100, 75));
 
-        //JButton colourButton = new JButton("Choose colour");
-
-        //ColourChooser myChooser = new ColourChooser();
-
-
-        ColourChooser myColourChooserButton = new ColourChooser();
-
-        myPanel.add(myColourChooserButton);
-
-        ActionListener myColourListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                myColour = JColorChooser.showDialog(MainFrame.this, "Choose a colour", myColour);
-
-                if (myColour == null) {
-                    myColour = Color.GREEN;
-                }
-            }
-        };
-
-        myColourChooserButton.colourButton.addActionListener(myColourListener);
-
-
-//        colourButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                myColour = JColorChooser.showDialog(MainFrame.this, "Choose a colour", myColour);
-//
-//                if (myColour == null) {
-//                    myColour = Color.GREEN;
-//                }
-//
-//            }
-//        });
-
-//        myPanel.add(colourButton);
+        colourButton.addActionListener(new ButtonListener());
+        myPanel.add(colourButton);
 
         ShapeChooser myShapeComboButton = new ShapeChooser();
         myPanel.add(myShapeComboButton);
-        
+
         //ActionListener listening to ShapeButton in myShapeComboButton
         ActionListener myListener = new ActionListener() {
             @Override
@@ -87,14 +52,14 @@ public class MainFrame extends JFrame {
                         System.out.println("selection variable is displaying: " + selection);
                         cl.show(cards, "oval");
                         break;
-                    
+
                     case "Line" :
                         selection = "Line";
                         System.out.println("Selected Line...");
                         System.out.println("selection variable is displaying: " + selection);
                         cl.show(cards, "line");
                         break;
-                        
+
                     default:
                         selection = "Unknown";
                         System.out.println("No match found.");
@@ -104,44 +69,38 @@ public class MainFrame extends JFrame {
                 }
             }
         };
-        
+
         myShapeComboButton.shapeButton.addActionListener(myListener);
 
-        
+
         LineThicknessChooser myThicknessChooser = new LineThicknessChooser();
         myPanel.add(myThicknessChooser);
-        
-        //JPanels for drawing different shapes depending on selection
-        Shape rectangle  = new Rectangle();
-        //rectangle.setBackground(Color.yellow);
-        // TODO colour should come from color chooser object.
-        rectangle.setColour(myColour);
-        
-        Shape oval = new Oval();
-        //oval.setBackground(Color.blue);
-        oval.setColour(myColour);
-        
-        Shape line = new Line();
-        //line.setBackground(Color.cyan);
-        line.setColour(myColour);
-        
-        JPanel nothing = new JPanel();
-        JLabel nothingLabel = new JLabel("Nothing to see here");
-        nothing.add(nothingLabel);
 
-        //cards for use in CardLayout
-        cards.add(oval, "oval");
-        cards.add(rectangle, "rectangle");
-        cards.add(line, "line");
-        cards.add(nothing, "nothing");
-        
-        
-        //BorderLayout in order to have buttons in a narrow strip on the top      
+        //BorderLayout in order to have buttons in a narrow strip on the top
         myFrame.add(myPanel, BorderLayout.NORTH);
         myFrame.add(cards, BorderLayout.CENTER);
         myFrame.setSize(800, 800);
         myFrame.setVisible(true);
 
+    }
+
+    public class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Color c = JColorChooser.showDialog(null, "Choose a Color", myColour);
+
+            Shape rectangle  = new Rectangle(c);
+            //rectangle.setBackground(c);
+            cards.add(rectangle, "rectangle");
+
+            Shape oval = new Oval(c);
+            //oval.setBackground(c);
+            cards.add(oval, "oval");
+
+            Shape line = new Line(c);
+            //line.setBackground(c);
+            cards.add(line, "line");
+
+        }
     }
 
 }
